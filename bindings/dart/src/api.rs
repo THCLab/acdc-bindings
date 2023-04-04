3a8890df75a015bd107be520e47ed002c5a45d74
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-04-04 11:30:07 +0200

    Update dependencies

--- bindings/dart/src/api.rs
+++ bindings/dart/src/api.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub use acdc::Attestation;
 use anyhow::{Context, Result};
 use flutter_rust_bridge::RustOpaque;
-use sai::derivation::SelfAddressing;
+use said::derivation::HashFunctionCode;
 
 pub struct ACDC(pub RustOpaque<Attestation>);
 
@@ -11,13 +11,14 @@ impl ACDC {
     pub fn new(issuer: String, schema: String, attributes: String) -> Result<ACDC> {
         let attributes: HashMap<String, String> = serde_json::from_str(&attributes)
             .with_context(|| "Attributes must be valid JSON".to_string())?;
-        let derivation = SelfAddressing::Blake3_256;
+        // TODO Stop using default hash function algorithm
+        let derivation = HashFunctionCode::Blake3_256;
         let attestation = Attestation::new(
             &issuer,
             schema
                 .parse()
-                .with_context(|| format!("Invalid schema SAI"))?,
-            derivation,
+                .with_context(|| "Invalid schema SAI".to_string())?,
+            derivation.into(),
             acdc::Attributes::Inline(attributes),
         );
         Ok(ACDC(RustOpaque::new(attestation)))
